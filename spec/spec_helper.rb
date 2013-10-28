10bfd97cad71968bb92b30b461b2df042691dc62
Michael Noack <michael@noack.com.au>
2013-10-29 10:08:27 +1030

    Add coverage requirements (57% is current coverage)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,8 @@
 require 'rubygems'
 require 'bundler/setup'
 
+MINIMUM_COVERAGE = 57
+
 if ENV['COVERAGE']
   require 'simplecov'
   require 'simplecov-rcov'
@@ -16,6 +18,14 @@ if ENV['COVERAGE']
     add_filter '/vendor/'
     add_filter '/spec/'
     add_group 'lib', 'lib'
+  end
+  SimpleCov.at_exit do
+    SimpleCov.result.format!
+    percent = SimpleCov.result.covered_percent
+    unless percent >= MINIMUM_COVERAGE
+      puts "Coverage must be above #{MINIMUM_COVERAGE}%. It is #{"%.2f" % percent}%"
+      Kernel.exit(1)
+    end
   end
 end
 
