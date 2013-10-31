487ad1207c3e1f105450bb863b1e1c185c986e72
Stefan Kojic <stefan.kojic@sealink.com.au>
2013-10-31 15:17:18 +1030

    Reworked validate_required_fields
     * Use CustomPresenceValidator which extends ActiveModel::Validator
     * Renamed validate_mandatory_fields -> validate_presence_by_custom_rules -
       which now accepts multiple params to support conditional validation

--- lib/rails_core_extensions/active_model_extensions.rb
+++ lib/rails_core_extensions/active_model_extensions.rb
@@ -1,3 +1,4 @@
+require 'active_model'
 module ActiveModelExtensions
   module Validations
     def self.included(base)
@@ -12,30 +13,35 @@ module ActiveModelExtensions
     # Example:
     # validate_required_fields "field1,field2 or field4"
     #
+
     # The string is a CSV of required field rules, where each field rule is:
     #  - the name of a required field
     #  - OR a set of required field names spearated by 'or' (where only ONE is required)
     #
-    def validate_required_fields(required_fields)
-      return if required_fields.empty?
+    class CustomPresenceValidator < ActiveModel::Validator
+      def validate(record)
+        required_fields = Array.wrap(@options[:attributes]).first.call
+        return if required_fields.empty?
 
-      required_fields.each do |required_field|
-        if required_field.include? ' or '
-          fields = required_field.split(' or ')
-          if fields.all? { |field| send(field).to_s.blank? }
-            errors.add(:base, "One of %s is required" % fields.map(&:humanize).to_sentence)
-          end
-        else
-          if send(required_field).to_s.blank?
-            errors.add(required_field, "is required")
+        required_fields.flatten.each do |required_field|
+          if required_field.include? ' or '
+            fields = required_field.split(' or ')
+            if fields.all? { |field| record.send(field).to_s.blank? }
+              record.errors.add(:base, "One of %s is required" % fields.map(&:humanize).to_sentence)
+            end
+          else
+            if record.send(required_field).to_s.blank?
+              record.errors.add(required_field, "is required")
+            end
           end
         end
+
       end
     end
 
     module ClassMethods
-      def validate_mandatory_fields(with)
-        validate { |m| m.validate_required_fields(with.call) }
+      def validate_presence_by_custom_rules(*with)
+        validates_with CustomPresenceValidator, _merge_attributes(with)
       end
     end
   end
