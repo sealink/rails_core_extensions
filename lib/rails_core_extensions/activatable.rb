145b3c512d82981533e5a9fdcaa5540b07eafe18
Michael Noack <michael@noack.com.au>
2014-03-17 11:32:15 +1030

    Use inherited_resources methods instead of make_resourceful
     * Works with QT4 now
     * Can't update QT3 to this rails_core_extensions now

--- lib/rails_core_extensions/activatable.rb
+++ lib/rails_core_extensions/activatable.rb
@@ -12,18 +12,18 @@ module Activatable
 
   module InstanceMethods
     def activate(success_block = nil)
-      current_object.active = params[:active] || false
-      current_object.save!
+      resource.active = params[:active] || false
+      resource.save!
       if success_block
         success_block.call
       else
-        flash[:success] = "#{current_object} #{params[:active] ? 'activated' : 'inactivated'}"
-        redirect_to(objects_path)
+        flash[:success] = "#{resource} #{params[:active] ? 'activated' : 'inactivated'}"
+        redirect_to(collection_path)
       end
     rescue ActiveRecord::ActiveRecordError => e
-      current_object.errors.add(:base, "Failed to inactivate: " + e.message)
-      flash[:error] = current_object.errors.full_messages.to_sentence
-      redirect_to(objects_path)
+      resource.errors.add(:base, "Failed to inactivate: " + e.message)
+      flash[:error] = resource.errors.full_messages.to_sentence
+      redirect_to(collection_path)
     end
   end
 
