43717cb54f80378a48ced4ce93d4f38050c07f52
Michael Noack <michael@noack.com.au>
2014-05-19 10:30:31 +0930

    Add spec

---
+++ spec/action_view_extensions_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_core_extensions/action_view_extensions'
+
+require 'action_view'
+
+describe RailsCoreExtensions::ActionViewExtensions do
+  before do
+    class TestModel1
+      include ActionView::Helpers::FormTagHelper
+      include ActionView::Helpers::FormOptionsHelper
+      include RailsCoreExtensions::ActionViewExtensions
+    end
+  end
+
+  after { Object.send(:remove_const, 'TestModel1') }
+
+  subject { TestModel1.new }
+
+  context '#boolean_select_tag' do
+    it 'should generate and have selected element selected' do
+      subject.boolean_select_tag('name', selected: '0').should ==
+        subject.select_tag('name', subject.options_for_select([['Yes', '1'], ['No', '0']], selected: '0'))
+    end
+  end
+end
