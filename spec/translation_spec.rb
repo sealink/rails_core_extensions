2019a0e7046825a8794d2a6acba3da3aa90a5244
Michael Noack <michael@noack.com.au>
2016-08-23 15:55:15 +0930

    Test translations

---
+++ spec/translation_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+require 'i18n'
+
+describe 'translations' do
+  let(:model_class) {
+    Class.new do
+      include ActiveRecordExtensions
+      include ActiveRecordExtensions::InstanceMethods
+
+      def initialize(name)
+        @name = name
+      end
+
+      def to_s
+        t('display_me', name: @name)
+      end
+    end
+  }
+
+  before do
+    stub_const 'TranslationModel', model_class
+  end
+
+  let(:class_translation) { TranslationModel.translate('display_me', name: 'Class') }
+  subject { TranslationModel.new('Ruby') }
+
+  specify { expect(subject.to_s).to eq 'My name is Ruby' }
+  specify { expect(class_translation).to eq 'My name is Class' }
+end
