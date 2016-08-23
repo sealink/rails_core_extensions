e549556a15e7c959590fe97bc75e2b7540f5f1af
Michael Noack <michael@noack.com.au>
2016-08-23 16:01:10 +0930

    Don't require activerecord
    * Rename to translation_key to simplify and avoid conflicts

--- lib/rails_core_extensions/translations.rb
+++ lib/rails_core_extensions/translations.rb
@@ -6,12 +6,25 @@ module RailsCoreExtensions
 
     module ClassMethods
       def translate(key, options = {})
-        I18n.translate key, options.merge(scope: base_class.name.tableize.singularize)
+        I18n.translate key, options.merge(scope: translation_key)
       end
 
-      def base_class
+      def translation_key
+        (base_activerecord_class || base_ruby_class).name.tableize.singularize
+      end
+
+      def base_activerecord_class
+        return unless defined?(ActiveRecord) && is_a?(ActiveRecord::Base)
         klass = self
-        while klass.superclass != ActiveRecord::Base && klass.superclass != Object
+        while klass.superclass != ActiveRecord::Base
+          klass = klass.superclass
+        end
+        klass
+      end
+
+      def base_ruby_class
+        klass = self
+        while klass.superclass != Object
           klass = klass.superclass
         end
         klass
