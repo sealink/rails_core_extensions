599e59db2f84af1facbf392d063147c6f2f1d2d9
Michael Noack <michael@noack.com.au>
2016-08-24 10:56:02 +0930

    Have to refactor as is_a? doesn't work on class level

--- lib/rails_core_extensions/translations.rb
+++ lib/rails_core_extensions/translations.rb
@@ -10,24 +10,20 @@ module RailsCoreExtensions
       end
 
       def translation_key
-        (base_activerecord_class || base_ruby_class).name.tableize.singularize
+        base_translation_class.name.tableize.singularize
       end
 
-      def base_activerecord_class
-        return unless defined?(ActiveRecord) && is_a?(ActiveRecord::Base)
+      def base_translation_class
         klass = self
-        while klass.superclass != ActiveRecord::Base
+        while !base_classes.include? klass.superclass
           klass = klass.superclass
         end
         klass
       end
 
-      def base_ruby_class
-        klass = self
-        while klass.superclass != Object
-          klass = klass.superclass
-        end
-        klass
+      def base_classes
+        return [ActiveRecord::Base, Object] if defined?(ActiveRecord)
+        [Object]
       end
 
       def t(key, options = {})
