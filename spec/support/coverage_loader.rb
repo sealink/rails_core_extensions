66228aaf2371c3090707fe14a4bad44bc8b5a090
Michael Noack <michael@noack.com.au>
2016-11-04 11:50:41 +1030

    Use coverage kit to enforce maximum coverage

--- spec/support/coverage_loader.rb
+++ spec/support/coverage_loader.rb
@@ -1,27 +1,4 @@
-MINIMUM_COVERAGE = 81.1
-
-unless ENV['COVERAGE'] == 'off'
-  require 'simplecov'
-  require 'simplecov-rcov'
-  require 'coveralls'
-  Coveralls.wear!
-
-  SimpleCov.formatters = [
-    SimpleCov::Formatter::RcovFormatter,
-    Coveralls::SimpleCov::Formatter
-  ]
-  SimpleCov.start do
-    add_filter '/vendor/'
-    add_filter '/spec/'
-    add_group 'lib', 'lib'
-  end
-  SimpleCov.at_exit do
-    SimpleCov.result.format!
-    percent = SimpleCov.result.covered_percent
-    puts "Coverage is #{"%.2f" % percent}%"
-    unless percent >= MINIMUM_COVERAGE
-      puts "Coverage must be above #{MINIMUM_COVERAGE}%"
-      Kernel.exit(1)
-    end
-  end
-end
+require 'simplecov-rcov'
+require 'coveralls'
+require 'coverage/kit'
+Coverage::Kit.setup(minimum_coverage: 81.1)
