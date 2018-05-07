372e9ed48d3ac3b806ce2f1d816e492a728bda5f
Michael Noack <michael@noack.com.au>
2018-05-07 16:14:24 +0930

    Address rubocop issues (quoting and long lines and trailing comma)

--- spec/action_view_extensions_spec.rb
+++ spec/action_view_extensions_spec.rb
@@ -1,6 +1,6 @@
-require 'rails_core_extensions/action_view_extensions'
+require "rails_core_extensions/action_view_extensions"
 
-require 'action_view'
+require "action_view"
 
 describe RailsCoreExtensions::ActionViewExtensions do
   before do
@@ -11,23 +11,29 @@ describe RailsCoreExtensions::ActionViewExtensions do
     end
   end
 
-  after { Object.send(:remove_const, 'TestModel1') }
+  after { Object.send(:remove_const, "TestModel1") }
 
   let(:helper) { TestModel1.new }
 
-  context '#boolean_select_tag' do
-    let(:yes_no) { [["Yes", "1"], ["No", "0"]] }
-    subject { helper.boolean_select_tag('name', args) }
+  context "#boolean_select_tag" do
+    let(:yes_no) { [%w[Yes 1], %w[No 0]] }
+    subject { helper.boolean_select_tag("name", args) }
 
-    context 'when elements selected' do
+    context "when elements selected" do
       let(:args) { { selected: 0 } }
-      let(:options) { helper.options_for_select(yes_no, selected: '0') }
+      let(:options) { helper.options_for_select(yes_no, selected: "0") }
 
-      it { is_expected.to eq helper.select_tag('name', options) }
+      it { is_expected.to eq helper.select_tag("name", options) }
 
-      context 'and other options passed' do
-        let(:args) { { selected: '0', include_blank: 'All' } }
-        it { is_expected.to eq helper.select_tag('name', options, include_blank: 'All') }
+      context "and other options passed" do
+        let(:args) { { selected: "0", include_blank: "All" } }
+        it {
+          is_expected.to eq helper.select_tag(
+            "name",
+            options,
+            include_blank: "All",
+          )
+        }
       end
     end
   end
