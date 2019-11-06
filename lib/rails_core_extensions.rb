3bc00a5f2d70f84845f2ec868ce696af74c8d768
John Mortlock <john.mortlock@gmail.com>
2019-11-06 19:49:36 +1030

    Remove support for Ruby 2.4 and Rails3

--- lib/rails_core_extensions.rb
+++ lib/rails_core_extensions.rb
@@ -28,17 +28,13 @@ module RailsCoreExtensions
     require 'rails_core_extensions/active_record_extensions'
     require 'rails_core_extensions/active_record_liquid_extensions'
     require 'rails_core_extensions/translations'
+    require 'rails_core_extensions/active_model_extensions'
 
     ActiveRecord::Base.send(:include, ActiveRecordCloning)
     ActiveRecord::Base.send(:include, ActiveRecordExtensions)
     ActiveRecord::Base.send(:include, RailsCoreExtensions::ActiveRecordLiquidExtensions)
     ActiveRecord::Base.send(:include, ActiveRecordExtensions::InstanceMethods)
     ActiveRecord::Base.send(:include, RailsCoreExtensions::Translations)
-
-    if ActiveRecord::VERSION::MAJOR >= 3
-      require 'rails_core_extensions/active_model_extensions'
-      ActiveRecord::Base.send(:include, ActiveModelExtensions::Validations)
-    end
+    ActiveRecord::Base.send(:include, ActiveModelExtensions::Validations)
   end
 end
-
