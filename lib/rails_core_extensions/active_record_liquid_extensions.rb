1a9c6a5b236758e9713fe1f29bbb3b3c0edd9719
john.mortlock <john.mortlock@gmail.com>
2022-07-02 10:15:46 +0930

    Correctly define the extension

--- lib/rails_core_extensions/active_record_liquid_extensions.rb
+++ lib/rails_core_extensions/active_record_liquid_extensions.rb
@@ -1,30 +1,30 @@
 module RailsCoreExtensions
   module ActiveRecordLiquidExtensions
     extend ActiveSupport::Concern
-  end
 
-  module ClassMethods
-    def validates_liquid(field)
-      field = field.to_sym
-      before_validation do |record|
-        begin
-          Liquid::Template.parse(record.send(field), error_mode: :strict)
-        rescue Liquid::SyntaxError => e
-          record.errors.add(field, "Liquid Syntax Error: #{e}")
+    module ClassMethods
+      def validates_liquid(field)
+        field = field.to_sym
+        before_validation do |record|
+          begin
+            Liquid::Template.parse(record.send(field), error_mode: :strict)
+          rescue Liquid::SyntaxError => e
+            record.errors.add(field, "Liquid Syntax Error: #{e}")
+          end
         end
       end
-    end
 
-    def liquid_field(field)
-      class_eval <<-CODE
-        def parsed_#{field}
-          Liquid::Template.parse(#{field})
-        end
+      def liquid_field(field)
+        class_eval <<-CODE
+          def parsed_#{field}
+            Liquid::Template.parse(#{field})
+          end
 
-        def render_#{field}(*args)
-          parsed_#{field}.render!(*args)
-        end
-      CODE
+          def render_#{field}(*args)
+            parsed_#{field}.render!(*args)
+          end
+        CODE
+      end
     end
   end
 end
